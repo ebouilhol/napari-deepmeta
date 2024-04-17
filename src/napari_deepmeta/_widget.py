37780926b36343949c3de77079acbf5987f29a58
ebouilhol <emmanuel.bouilhol@gmail.com>
2024-04-17 16:46:22 +0200

    Update _widget.py

--- src/napari_deepmeta/_widget.py
+++ src/napari_deepmeta/_widget.py
@@ -71,6 +71,10 @@ def show_shapes_3D(viewer, plottable, color, text):
             opacity=0.6,
             name=text,
         )
+        labels = layer.to_labels([128,128])
+        labels_layer = viewer.add_labels(labels, name=text)
+        labels_layer.visible = True
+        
     except Exception as e:
         print(e)
         print(np.shape(plottable))
